47790c895b8b034a3a0a9d1c90d0a0d477fb87a6
oneRain <onerain88@gmail.com>
2016-04-12 11:15:51 +0800

    完成教程3

---
+++ T3_First_Triangle/Source.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "ogldev_math_3d.h"
+
+GLuint VBO;
+
+void RenderSceneCB()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(0);
+
+	glutSwapBuffers();
+}
+
+void InitializeGlutCallbacks()
+{
+	glutDisplayFunc(RenderSceneCB);
+}
+
+void CreateVertexBuffer()
+{
+	Vector3f vertices[3];
+	vertices[0] = Vector3f(-0.5f, -0.5f, 0.0f);
+	vertices[1] = Vector3f(0.5f, -0.5f, 0.0f);
+	vertices[2] = Vector3f(0.0f, 0.5f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 03");
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	glClearColor(0.8f, 0.3f, 0.1f, 1.0f);
+
+	InitializeGlutCallbacks();
+
+	CreateVertexBuffer();
+
+	glutMainLoop();
+
+	return 0;
+}
